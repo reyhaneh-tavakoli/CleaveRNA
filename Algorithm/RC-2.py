d5e59f232e31e7a9667724f4e14343813e1b3f85
Martin Raden <mmann@informatik.uni-freiburg.de>
2024-12-11 10:22:46 +0100

    no overlapping interaction candidates

--- Algorithm/RC-2.py
+++ Algorithm/RC-2.py
@@ -97,6 +97,7 @@ def construct_intarna_command(query_file, target_file, parameter_file, additiona
         f"--outMode C "
         f"--outMaxE -4 "
         f"--outNumber 2 "
+        f"--outOverlap=N " 
         f"--outCsvCols 'id1,seq1,id2,seq2,E,Etotal,ED1,ED2,Pu1,Pu2,subseqDB,hybridDB,Pu2,E_dangleL,E_dangleR,E_endL,E_endR,E_init,E_loops,E_hybrid,E_norm,E_hybridNorm,E_add,seedStart1,seedEnd1,seedStart2,seedEnd2,seedE,seedED1,seedED2,seedPu1,Eall2,Zall,Zall1,Zall2,EallTotal,seedPu2,w,Eall,Eall1,P_E,RT' "
     )
     return base_command + additional_params
