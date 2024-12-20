b43750fe7e71f2bb3a6e11c89d430867d0a3f082
Martin Raden <mmann@informatik.uni-freiburg.de>
2024-12-20 10:47:37 +0100

    Update script location

--- Algorithm/SARS-CoV-2/test/run.sh
+++ Algorithm/SARS-CoV-2/test/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # run test call for data within this directory
-python ../script/RNAcutter.py -motifs=AU,GU -LA=9 -RA=16 -target=target.fa
+python ../../../RNAcutter.py -motifs=AU,GU -LA=9 -RA=16 -target=target.fa
